0a630680bc0df590433e465a64ce6e1e9dc82968
Nhu Trinh <nqtrinh87@gmail.com>
2019-06-05 14:50:50 -0700

    added top ten list and removed functions from js

--- js/app.js
+++ js/app.js
@@ -7,60 +7,59 @@ var correctCounter = 0;
 
 // Array for questions
 var questions = [
-  'First truth/lie: I used to be a poker dealer, Y/N?',
-  'Second truth/lie: I\'m so fit I ran a marathon. Y/N?',
-  'Third truth/lie: Just like the rest of the PNW, I LOVE snowboarding! Y/N?',
-  'Fourth truth/lie: I have jumped out of a plan before. Y/N?',
-  'Fifth truth/lie: Here\'s an easy one for you, because you\'re not so good at this game, I LOVE the outdoors. Y/N?'
+  'I used to be a poker dealer. Y/N?',
+  'I\'m so fit I ran a marathon. Y/N?',
+  'I LOVE snowboarding! Y/N?',
+  'I have jumped out of a plan before. Y/N?',
+  'Here\'s an easy one for you, I LOVE the outdoors. Y/N?',
+  'How old am I?', // indicate whether the guess is too high or too low, 4 guesses
+  'Can you guess a state that I have lived in besides Washington?' //multiple correct answers to be picked, user gets 6 tries, displays all of the possible correct answers.
 ]
 
 // Begin game
-var username = prompt('Let\'s play four truths and a lie! But first what\'s your name?');
+var username = prompt('Let\'s see if you know me! But first what\'s your name?');
 if(username === 'null' || ''){
   username = 'Anonymous';
 } 
-alert(`Yay! Let's play ${username}, I'm pretty cool, you'll see!`);
+alert(`Yay! Let's play ${username}!`);
 
 
 // Gets responses from all questions
-var getAnswers = function() {
-  for (var i = 0; i < questions.length; i++){
-    response = prompt(questions[i]).toLowerCase();
-    console.log('before changing the response:', response)
-    if(response === 'y'){
-      response = 'yes'
-    }
-    if(response === 'n'){
-      response = 'no'
-    }
-    console.log('after changing the response:', response)
-    userAnswers.push(response);
+for (var i = 0; i < questions.length; i++){
+  response = prompt(questions[i]).toLowerCase();
+  console.log('before changing the response:', response)
+  if(response === 'y'){
+    response = 'yes'
   }
+  if(response === 'n'){
+    response = 'no'
+  }
+  console.log('after changing the response:', response)
+  userAnswers.push(response);
 }
-getAnswers();
+
 
 // Counts the number of correct answers
-var counter = function() {
-  for (var i = 0; i < correctAnswers.length; i++){
-    if(userAnswers[i] === correctAnswers[i]){
-      correctCounter++;
-    } 
-  }
+
+for (var i = 0; i < correctAnswers.length; i++){
+  if(userAnswers[i] === correctAnswers[i]){
+    correctCounter++;
+  } 
 }
-counter();
+
 
 
 var results = document.getElementById('result-message');
 
 // Checks to see if all answers are correct and displays results
-var checkForWin = function(){
-  if(correctCounter === 5){
-    alert('Congratulations! You got 5 out of 5 correct!')
-    results.textContent = (`A little creepy how well you know me ${username}...`)
-  } else {
-    alert('You\'re not very good at this game...')
-    results.textContent = (`${username}, I can't say I'm not disappointed, you should study-up.`);
-  }
+
+if(correctCounter === 5){
+  alert('Congratulations! You got 5 out of 5 correct!')
+  results.textContent = (`A little creepy how well you know me ${username}...`)
+} else {
+  alert('You\'re not very good at this game...')
+  results.textContent = (`${username}, I can't say I'm not disappointed, you should study-up.`);
 }
 
-checkForWin();
+
+

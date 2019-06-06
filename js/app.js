f4ab0ad1a4b4d449635497772deb9176466924e6
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-06-06 14:29:22 -0700

    Functions are my drizzzgit add .

--- js/app.js
+++ js/app.js
@@ -17,77 +17,88 @@ var questions = [
 var username = prompt('Let\'s see if you know me! But first, what\'s your name?');
 alert(`Yay! Let's play ${username}!`);
 
-
-// Gets responses from the five questions and display their results
-for (var i = 0; i < questions.length; i++){
-  response = prompt(questions[i]).toLowerCase();
-  console.log('before changing the response:', response);
-  if(response === 'y'){
-    response = 'yes';
-    if(response === correctAnswers[i]){
-      alert('Correct!');
-      correctCounter++;
-    } else {
-      alert('Wrong!');
+var fiveInitialQuestions = function() {
+  // Gets responses from the five questions and display their results
+  for (var i = 0; i < questions.length; i++){
+    response = prompt(questions[i]).toLowerCase();
+    console.log('before changing the response:', response);
+    if(response === 'y'){
+      response = 'yes';
+      if(response === correctAnswers[i]){
+        alert('Correct!');
+        correctCounter++;
+      } else {
+        alert('Wrong!');
+      }
     }
-  }
-  if(response === 'n'){
-    response = 'no';
-    if(response === correctAnswers[i]){
-      alert('Correct!');
-      correctCounter++;
-    } else {
-      alert('Wrong!');
+    if(response === 'n'){
+      response = 'no';
+      if(response === correctAnswers[i]){
+        alert('Correct!');
+        correctCounter++;
+      } else {
+        alert('Wrong!');
+      }
     }
+    console.log('after changing the response:', response);
   }
-  console.log('after changing the response:', response);
 }
 
+fiveInitialQuestions();
 
 // Numeric Question with 4 tries, indicate whether the guess is too high or too low
 var tries = 4;
-var ageQuestion;
-while(tries > 0 && ageQuestion !== 32){
-  ageQuestion = prompt('How old am I?');
-  if(tries === 0){
-    alert(`Sorry you have ${tries} left, next question!`);
-  } else if(ageQuestion > 32){
-    tries--;
-    alert(`Rude! Too high! Guess again, you have ${tries} more tries!`);
-  } else if(ageQuestion < 32){
-    tries--;
-    alert(`Thanks! But too low! you have ${tries} more tries!`);
-  } else {
-    alert('Nice! You guessed correctly!');
-    correctCounter++;
-    tries = 0;
+var ageAnswer;
+
+var ageQuestion = function() {
+  while(tries > 0 && ageAnswer !== 32){
+    ageAnswer = prompt('How old am I?');
+    if(tries === 0){
+      alert(`Sorry you have ${tries} left, next question!`);
+    } else if(ageAnswer > 32){
+      tries--;
+      alert(`Rude! Too high! Guess again, you have ${tries} more tries!`);
+    } else if(ageAnswer < 32){
+      tries--;
+      alert(`Thanks! But too low! you have ${tries} more tries!`);
+    } else {
+      alert('Nice! You guessed correctly!');
+      correctCounter++;
+      tries = 0;
+    }
   }
 }
 
-
+ageQuestion();
 
 // Multiple correct answers to be picked, user gets 6 guesses, display all of the possible correct answers.
 var guesses = 6;
-var stateQuestion;
-while (guesses > 0){
-  stateQuestion = prompt('Can you guess a state that I have lived in besides WASHINGTON?').toUpperCase();
-  if(stateQuestion === correctAnswers[6][0] || stateQuestion === correctAnswers[6][1]){
-    alert(`Whoa! You're right, I've lived in WASHINGTON, as well as these states: ${correctAnswers[6]}`);
-    correctCounter++;
-    guesses = 0;
-    break;
-  } else {
-    alert(`Wrong! Try again, you have ${guesses} guesses left.`);
-    guesses--;
+var stateAnswer;
+
+var stateQuestion = function() {
+  while (guesses > 0){
+    stateAnswer = prompt('Can you guess a state that I have lived in besides WASHINGTON?').toUpperCase();
+    if(stateAnswer === correctAnswers[6][0] || stateAnswer === correctAnswers[6][1]){
+      alert(`Whoa! You're right, I've lived in WASHINGTON, as well as these states: ${correctAnswers[6]}`);
+      correctCounter++;
+      guesses = 0;
+      break;
+    } else {
+      alert(`Wrong! Try again, you have ${guesses} guesses left.`);
+      guesses--;
+    }
   }
 }
 
+stateQuestion();
 
 // Checks to see how many the user got correct.
-if(correctCounter === 7){
-  alert(`Congratulations, ${username}! You got ${correctCounter} out of 7 correct!`);
-} else {
-  alert(`Womp, womp. ${username}, you only got ${correctCounter} out of 7 correct.`);
+var checkWinConditions = function() {
+  if(correctCounter === 7){
+    alert(`Congratulations, ${username}! You got ${correctCounter} out of 7 correct!`);
+  } else {
+    alert(`Womp, womp. ${username}, you only got ${correctCounter} out of 7 correct.`);
+  }
 }
 
 

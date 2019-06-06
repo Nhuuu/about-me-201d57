9bbe1c36f3508e1b37a6abfc768330dbb3e97451
Nhu Trinh <nqtrinh87@gmail.com>
2019-06-05 20:18:20 -0700

    question 7 is complete

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 'use strict';
 
 var response;
-var correctAnswers = ['yes', 'yes', 'no', 'yes', 'yes', 32, ['Texas', 'Louisiana']];
-var userAnswers = [];
+var correctAnswers = ['yes', 'yes', 'no', 'yes', 'yes', 32, ['TEXAS', 'LOUISIANA']];
 var correctCounter = 0;
 
 // Array for questions
@@ -20,28 +19,29 @@ alert(`Yay! Let's play ${username}!`);
 
 
 // Gets responses from the five questions and display their results
-// for (var i = 0; i < questions.length; i++){
-//   response = prompt(questions[i]).toLowerCase();
-//   console.log('before changing the response:', response);
-//   if(response === 'y'){
-//     response = 'yes';
-//     if(response === correctAnswers[i]){
-//       alert('Correct!')
-//     } else {
-//       alert('Wrong!')
-//     }
-//   }
-//   if(response === 'n'){
-//     response = 'no';
-//     if(response === correctAnswers[i]){
-//       alert('Correct!');
-//     } else {
-//       alert('Wrong!');
-//     }
-//   }
-//   console.log('after changing the response:', response);
-//   userAnswers.push(response);
-// }
+for (var i = 0; i < questions.length; i++){
+  response = prompt(questions[i]).toLowerCase();
+  console.log('before changing the response:', response);
+  if(response === 'y'){
+    response = 'yes';
+    if(response === correctAnswers[i]){
+      alert('Correct!')
+      correctCounter++;
+    } else {
+      alert('Wrong!')
+    }
+  }
+  if(response === 'n'){
+    response = 'no';
+    if(response === correctAnswers[i]){
+      alert('Correct!');
+      correctCounter++;
+    } else {
+      alert('Wrong!');
+    }
+  }
+  console.log('after changing the response:', response);
+}
 
 
 // Numeric Question with 4 tries, indicate whether the guess is too high or too low
@@ -51,8 +51,6 @@ while(tries > 0 && ageQuestion !== 32){
   ageQuestion = prompt('How old am I?');
   if(tries === 0){
     alert(`Sorry you have ${tries} left, next question!`);
-    userAnswers.push(ageQuestion);
-    console.log(userAnswers);
   } else if(ageQuestion > 32){
     tries--;
     alert(`Rude! Too high! Guess again, you have ${tries} more tries!`);
@@ -61,46 +59,37 @@ while(tries > 0 && ageQuestion !== 32){
     alert(`Thanks! But too low! you have ${tries} more tries!`);
   } else {
     alert('Nice! You guessed correctly!');
-    userAnswers.push(ageQuestion);
-    console.log(userAnswers);
+    correctCounter++;
     tries = 0;
   }
 }
 
 
 
-
-
-// TODO: multiple correct answers to be picked, user gets 6 tries, displays all of the possible correct answers. Just have to guess one of the multiple choices available.
-// var guesses = 6;
-// var stateQuestion = prompt('Can you guess a state that I have lived in besides Washington?')
-// while (guesses <= 6 ){
-//   if(stateQuestion === 'Texas'){
-//     alert(`Whoa! You're right, I've lived in ${stateQuestion} as well as Washington and ${correctAnswers[7][1]}`);
-//     guesses--
-//   } else if (stateQuestion === 'Louisiana'){
-//     alert(`Whoa! You're right, I've lived in ${stateQuestion} as well as Washington and ${correctAnswers[7][0]}`);
-//     guesses--
-//   } else {
-//     alert(`Wrong! Try again, you have ${guesses} guesses left`);
-//   }
-// }
-
-
-
-// TODO: Fix this, bc the last question. Counts the number of correct answers
-for (var i = 0; i < correctAnswers.length; i++){
-  if(userAnswers[i] === correctAnswers[i]){
-    correctCounter++;
-  } 
+// Multiple correct answers to be picked, user gets 6 guesses, display all of the possible correct answers.
+var guesses = 6;
+var stateQuestion;
+while (guesses > 0){
+  stateQuestion = prompt('Can you guess a state that I have lived in besides WASHINGTON?').toUpperCase();
+  for(var i = 0; i < correctAnswers[6].length; i++){
+    if(stateQuestion === correctAnswers[6][i]){
+      alert(`Whoa! You're right, I've lived in WASHINGTON, as well as these states: ${correctAnswers[6]}`);
+      correctCounter++;
+      guesses = 0;
+      break;
+    } else {
+      alert(`Wrong! Try again, you have ${guesses} guesses left.`);
+      guesses--;
+    }
+  }
 }
 
 
 // Checks to see how many the user got correct.
 if(correctCounter === 7){
-  alert(`Congratulations! You got ${correctCounter} out of 7 correct!`)
+  alert(`Congratulations, ${username}! You got ${correctCounter} out of 7 correct!`)
 } else {
-  alert(`You're not very good at this game...you only got ${correctCounter} out of 7 correct.`)
+  alert(`Womp, womp. ${username}, you only got ${correctCounter} out of 7 correct.`)
 }
 
 
